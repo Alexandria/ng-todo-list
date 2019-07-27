15029cadb1075f94fbb0b9c39e7f47a429d7bc35
Lex <Lexluka1031@gmail.com>
2019-07-27 15:38:41 -0500

    Update package json

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -12,7 +12,7 @@ import { Component, OnInit, Output, EventEmitter } from "@angular/core";
     />
     <button class="btn" (click)="submitValue(inputElementRef.value)">
       Save
-    </button>
+    </button> 
   `,
   styleUrls: ["./input-button-unit.component.css"]
 })
